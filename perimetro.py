a5ca07c64778d6c849107157093f27dfcd753a23
Massimiliano Greco <102535941+xMaaMx@users.noreply.github.com>
2022-07-18 16:21:48 +0200

    Add files via upload

--- perimetro.py
+++ perimetro.py
@@ -1,32 +1,43 @@
 def perimetro (): #la funziona verrà eseguita richiamandola, senza uscire dal programma.
 				      #l'utente in base al suo input numerico,selezionerà l'operazione da effettuare.
-		print ("il programma calcola il perimetro di una figura geometrica"      )
-		print ("""
-		- Quadrato >>1
-		- Rettangolo >>2
-		- Cerchio >> 3"""   )
+		scelta =-1
+		while (scelta != 0): #ciclato il menu
+			print ("il programma calcola il perimetro di una figura geometrica"      )
+			print ("""
+			- Quadrato >>1
+			- Rettangolo >>2
+			- Cerchio >> 3
+			- Triangolo Isoscele    >> 4 """   )
 
 #inizio esercizio
-		pigreco = 3.14 #creata una variabile per non ripetere il valore numerico del pigreco durante l'esercizio.(ottimizzazione del codice)
+			pigreco = 3.14 #creata una variabile per non ripetere il valore numerico del pigreco durante l'esercizio.(ottimizzazione del codice)
 
-		print ("Inserire la scelta:" )
-		scelta = int (input ("  "))
-		if scelta == 1:
-			print ("Hai selezionato il perimetro del Quadrato")
-			lato = float(input("Inserisci il valore del lato del quadrato"     ))
-			print ("il perimetro del quadrato, avente lato"    ,lato, "è:", lato *4)
-		elif scelta == 2:
-			print ("Hai selezionato il perimetro del rettangolo"      )
-			base = float (input("Inserisci il valore della base"    ))
-			altezza = float (input("Inserisci il valore della base"    ))
-			print ("Il perimetro del Rettangolo, avente base", base, "e altezza", altezza, "è:",base *2 + 2*altezza)
-		elif scelta == 3:
-			print ("Hai selezionato il perimetro del raggio "     )
-			raggio = float(input("Inserisci il valore del raggio"     ))
-			perimetro =( (raggio*raggio)*pigreco)
-			print("Il perimetro del cerchio é dato da  : "     ,raggio, "per " ,raggio, "per" ,pigreco," ed è uguale a:"   , perimetro)
-		else:
-			print ("Inserire una scelta valida"   )
+			print ("Inserire la scelta:" )
+			scelta = int (input ("  "))
+			if scelta == 1:
+				print ("Hai selezionato il perimetro del Quadrato")
+				lato = float(input("Inserisci il valore del lato del quadrato"     ))
+				print ("il perimetro del quadrato, avente lato"    ,lato, "è:", lato *4)
+			elif scelta == 2:
+				print ("Hai selezionato il perimetro del rettangolo"      )
+				base = float (input("Inserisci il valore della base"    ))
+				altezza = float (input("Inserisci il valore della base"    ))
+				print ("Il perimetro del Rettangolo, avente base", base, "e altezza", altezza, "è:",base *2 + 2*altezza)
+			elif scelta == 3:
+				print ("Hai selezionato il perimetro del raggio "     )
+				raggio = float(input("Inserisci il valore del raggio"     ))
+				perimetrocerchio =( (raggio*raggio)*pigreco)
+				print("Il perimetro del cerchio é dato da  : "     ,raggio, "per " ,raggio, "per" ,pigreco," ed è uguale a:"   , perimetrocerchio)
+			elif scelta == 4:
+				print ("Hai selezionato il perimetro del triangolo"     )
+				print ("NB: il triangolo isoscele prevede due lati su tre di egual misura"      )
+				lato1 = float(input("Inserisci il valore del primo lato"   ))
+				lato2= float(input("Inseriscil il valore del secondo lato"    ))
+				lato3=float(input("Inserisci il valore del terzo lato"    ))
+				perimetrotriangolo = (lato1*lato2*lato3)
+				print ("il perimetro del triangolo isoscele è dato da:"      ,lato1,  "*" ,lato2, "*" ,lato3, ".Il risultato è:"  , perimetrotriangolo)
+			else:
+				print ("Inserire una scelta valida"   )
 
 perimetro();
 #in questo modo richiamiamo la funzione iniziale del perimetro.
